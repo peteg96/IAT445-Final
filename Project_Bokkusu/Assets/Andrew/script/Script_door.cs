b45ca838621cf1a796f635e44a110927d867d28c
1105959326 <69542207+1105959326@users.noreply.github.com>
2020-11-05 20:19:14 -0800

    forest

---
+++ Project_Bokkusu/Assets/Andrew/script/Script_door.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Script_door : MonoBehaviour
+{
+    Animator animator;
+    bool doorOpen;
+    public GameObject UI;
+    
+    void Start()
+    {
+        doorOpen = false;
+        UI.SetActive(false);
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            UI.SetActive(true);//if player in the area display the UI
+        }
+        if (col.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            doorOpen = true;
+            DoorControl("Open");
+            //if player in the area && press E open the door
+        }
+    }
+  
+    void OnTriggerExit(Collider col)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            DoorControl("Close");
+            //if player leave the area close the door
+        }
+        UI.SetActive(false);
+        //if player leave the area, remove the UI
+    }
+    void DoorControl (string direction)
+    {
+        animator.SetTrigger(direction);
+    }//function as switch
+}
